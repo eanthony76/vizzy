2e1f9b4db2ae24489ab6f7263bf2faa60c343f7a
eanthony76 <anthonyevanm@gmail.com>
2023-02-21 08:56:12 -0500

    changes to package

--- vizzy-package/setup.py
+++ vizzy-package/setup.py
@@ -14,7 +14,7 @@ with open('HISTORY.md') as history_file:
 
 setup_args = dict(
     name='vizzy',
-    version='0.2.7',
+    version='0.2.71',
     description='Useful tools to visualize NLP data',
     long_description_content_type="text/markdown",
     long_description=README + '\n\n' + HISTORY,
@@ -42,7 +42,7 @@ install_requires = [
     'spacy',
     'textstat',
     'gensim',
-    'pyldavis'
+    'scikit-learn'
 ]
 
 if __name__ == '__main__':
